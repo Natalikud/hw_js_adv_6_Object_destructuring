a9c4e101bbbe423ea861e8d971b79b30d45a7a0b
Natalikud2 <Reimova091@mail.ru>
2023-08-07 12:42:11 +0800

    Commit 2. Modified app

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,17 @@ export default function specialAttack(obj) {
   const { special } = obj;
   const resultList = [];
   special.forEach((element) => {
-    if (element.description === undefined) {
-      element.description = 'Описание недоступно';
-      resultList.push(element);
+    const {
+      id, name, icon, description,
+    } = element;
+    if (description === undefined) {
+      resultList.push({
+        id, name, icon, description: 'Описание недоступно',
+      });
     } else {
-      resultList.push(element);
+      resultList.push({
+        id, name, icon, description,
+      });
     }
   });
   return resultList;
